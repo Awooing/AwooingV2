452c8295cd3028bd5b498475a6dd70dcb48d2584
VottusCode <Listopad_22>
2020-05-02 00:59:38 +0200

    Added assets

---
+++ public/static/js/update.js
@@ -0,0 +1,54 @@
+function update() {
+    $.getJSON("https://api.awooing.moe/votes", function (data) {
+
+        const total =
+              parseInt(data.tokiVotes)
+            + parseInt(data.rinVotes)
+            + parseInt(data.vockVotes)
+            + parseInt(data.spaghettVotes)
+            + parseInt(data.muffinVotes)
+            + parseInt(data.vottusVotes);
+
+        // Tokichii
+        const toki = parseInt(data.tokiVotes);
+        setVote($("#tokiVotes"), $("#tokiVotesTotal"), ((toki / total) * 100), toki, "Tokichii");
+        // Rin
+        const rin = parseInt(data.rinVotes)
+        setVote($("#rinVotes"), $("#rinVotesTotal"), ((rin / total) * 100), rin, "Rin");
+        // Vocktile
+        const vock = parseInt(data.vockVotes);
+        setVote($("#vockVotes"), $("#vockVotesTotal"), ((vock / total) * 100), vock, "Vocktile");
+        // SpaghettO
+        const spaghettO = parseInt(data.spaghettVotes);
+        setVote($("#spaghettVotes"), $("#spaghettVotesTotal"), ((spaghettO / total) * 100), spaghettO, "Spaghett");
+        // Muffin
+        const muffin = parseInt(data.muffinVotes);
+        setVote($("#muffinVotes"), $("#muffinVotesTotal"), ((muffin / total) * 100), muffin, "Muffin");
+        // Vottus
+        const vottus = parseInt(data.vottusVotes);
+        setVote($("#vottusVotes"), $("#vottusVotesTotal"), ((vottus / total) * 100), vottus, "Vottus");
+        // Total
+        $("#total").text(total);
+
+        setTimeout(update, 40000);
+    });
+}
+
+function correctS(count) {
+    if (count === 1) {
+        return " Vote";
+    } else {
+        return " Votes";
+    }
+}
+
+function setVote(bar, total, percent, votes, name) {
+    bar.attr("style", "width:" + percent + "%");
+    bar.attr("aria-valuenow", percent);
+    bar.text( votes + correctS(votes));
+    total.attr("style", "width:" + percent + "%");
+    total.attr("aria-valuenow", percent);
+    total.text(name + " (" + votes + ")");
+}
+
+update();
--- src/components/pages/AwooPics.vue
+++ src/components/pages/AwooPics.vue
@@ -4,6 +4,7 @@
         <div class="container">
             <div class="awoo-wrapper" style="text-align-last: center;">
                 <p>Every time you refresh the page, you'll see a new <img alt='Awooo' class='awoo' src='https://cdn.discordapp.com/emojis/322522663304036352.png?v=1'></p>
+                <!-- todo: remove text on image load -->
                 <h3 id="awoos-loading">Loading awoos....</h3>
                 <img id="awoos-image" :src="'https://cdn.awooing.moe/' + this.response.path" alt="We encountered an awooror. Please refresh.">
             </div>
@@ -24,13 +25,14 @@
                 response: null
             }
         },
-        mounted() {
+        async mounted() {
             topbar.config({barColors: {0:"#281483", .3:"#8f6ed5", 1.0:"#d782d9"}})
             topbar.show()
             axios.get('https://awooing.moe/api/v1/awoo')
                 .then(res => (this.response = res.data))
                 .then(topbar.hide)
-                .then(document.getElementById("awoos-loading").remove);
+            await document.getElementById("awoos-loading").remove()
+            // todo: doesnt work please help or ill die here
         }
     }
 </script>
