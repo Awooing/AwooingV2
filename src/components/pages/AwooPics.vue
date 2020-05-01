758106dbc8b4580b6ace281494a03810392e6988
Niputi <7137178+Niputi@users.noreply.github.com>
2020-05-02 01:06:54 +0200

    small fixes - removed unused deps - made awoo pics nicer - added missing images

--- src/components/pages/AwooPics.vue
+++ src/components/pages/AwooPics.vue
@@ -3,10 +3,11 @@
         <Header :title="this.title"/>
         <div class="container">
             <div class="awoo-wrapper" style="text-align-last: center;">
-                <p>Every time you refresh the page, you'll see a new <img alt='Awooo' class='awoo' src='https://cdn.discordapp.com/emojis/322522663304036352.png?v=1'></p>
-                <!-- todo: remove text on image load -->
-                <h3 id="awoos-loading">Loading awoos....</h3>
-                <img id="awoos-image" :src="'https://cdn.awooing.moe/' + this.response.path" alt="We encountered an awooror. Please refresh.">
+                <p>Every time you refresh the page or click on the image <br> you'll see a new 
+                    <img alt='Awooo' class='awoo' src='https://cdn.discordapp.com/emojis/322522663304036352.png?v=1'>
+                </p>
+                <h3 id="awoos-loading" v-if="imageSrc == null">Loading awoos....</h3>
+                <img id="awoos-image" v-if="imageSrc !== null" @click="this.loadImage" :src="imageSrc" alt="We encountered an awooror. Please refresh.">
             </div>
         </div>
     </div>
@@ -22,17 +23,20 @@
         data() {
             return {
                 title: "Awoo Pics",
-                response: null
+                imageSrc: null
             }
         },
-        async mounted() {
+        mounted() {
             topbar.config({barColors: {0:"#281483", .3:"#8f6ed5", 1.0:"#d782d9"}})
-            topbar.show()
-            axios.get('https://awooing.moe/api/v1/awoo')
-                .then(res => (this.response = res.data))
-                .then(topbar.hide)
-            await document.getElementById("awoos-loading").remove()
-            // todo: doesnt work please help or ill die here
+            this.loadImage()
+        },
+        methods: {
+            loadImage: function() {
+                topbar.show()
+                axios.get('https://awooing.moe/api/v1/awoo')
+                    .then(res => (this.imageSrc = "https://cdn.awooing.moe/" + res.data.path))
+                    .then(topbar.hide())
+            }
         }
     }
 </script>
